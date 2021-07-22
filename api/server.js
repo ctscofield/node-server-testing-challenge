20fdb3b651de8d3a0f704d68cb2f7c2f66a05e00
ctscofield <ctscofield@gmail.com>
2021-07-22 15:09:32 -0600

    set up

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+//[GET]
+
+//[POST]
+
+//[DELETE]
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
